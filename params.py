d0f48703e6cbda7405880624a996ee6f1b187ae0
jmakieve <44616576+jmakieve@users.noreply.github.com>
2018-11-08 18:42:02 -0800

    Create params.py

---
+++ params.py
@@ -0,0 +1,13 @@
+fission_rate = 0
+energy_per_fission = 0
+fuel_mass = 0
+avg_product_mass = 0
+avg_product_charge = 0
+
+equilibrium_shell_temp = 0
+shell_material = ''
+
+thrust = 0
+specific_impulse = 0
+delta_v = 0
+
